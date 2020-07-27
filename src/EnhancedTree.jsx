db1e48251c5a78a1e5b8a8598b7763178824121e
Dayton Wilson <jdwilson73@Daytons-MacBook-Pro.local>
2020-07-27 11:54:10 -0400

    Can click branch node to add test branch child

--- src/EnhancedTree.jsx
+++ src/EnhancedTree.jsx
@@ -3,8 +3,11 @@ import Tree from 'react-tree-graph';
 import { findFirst } from 'obj-traverse/lib/obj-traverse';
 
 export default class EnhancedTree extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { data, height, width, svgProps } = this.props;
+    let count = 0;
+    this.state = {data, height, width, svgProps, count};
   }
 
 /*
@@ -14,19 +17,30 @@ export default class EnhancedTree extends React.Component {
   }*/
 
   render() {
-    const { data, height, width, svgProps } = this.props;
-
+    //const { data, height, width, svgProps } = this.props;
+    const { data, height, width, svgProps, count } = this.state;
     const logTest = (e, node) => {
       e.preventDefault();
       alert(`Spooooop ${node}`);
 
       let obj = findFirst(data, 'children', { name: node });
-      obj.children.push({
-        name: 'Test',
-        textProps: {transform: 'rotate(90)'},
-      });
+      if (!obj.children) {
+        obj.children = [{
+          name: `Test ${count}`,
+          textProps: {transform: 'rotate(90)'},
+        }];
+        this.setState({count: count + 1});
+      }
+      else {
+        obj.children.push({
+          name: `Test ${count}`,
+          textProps: {transform: 'rotate(90)'},
+        });
+        this.setState({count: count + 1});
+      }
       console.log(obj);
       console.log(data);
+      this.setState({data: data});
     }
 
     return(
