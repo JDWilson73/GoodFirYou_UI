229c19f1bdbe5077c5b0fe58e61a8ac09dcec8e9
Dayton Wilson <jdwilson73@Daytons-MacBook-Pro.local>
2020-07-27 11:12:18 -0400

    Added obj-traverse dependency, proto-adding of children to tree--needs work

--- src/EnhancedTree.jsx
+++ src/EnhancedTree.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import Tree from 'react-tree-graph';
-
+import { findFirst } from 'obj-traverse/lib/obj-traverse';
 
 export default class EnhancedTree extends React.Component {
   constructor() {
     super();
   }
 
+/*
   logTest(e, node) {
     e.preventDefault();
     alert(`Spooooop ${node}`);
-  }
+  }*/
+
   render() {
     const { data, height, width, svgProps } = this.props;
+
+    const logTest = (e, node) => {
+      e.preventDefault();
+      alert(`Spooooop ${node}`);
+
+      let obj = findFirst(data, 'children', { name: node });
+      obj.children.push({
+        name: 'Test',
+        textProps: {transform: 'rotate(90)'},
+      });
+      console.log(obj);
+      console.log(data);
+    }
+
     return(
       <div className="custom-container">
         <h1>Placeholder for the LandingPage</h1>
@@ -23,7 +39,7 @@ export default class EnhancedTree extends React.Component {
           svgProps={svgProps}
           gProps={{
             className: 'node',
-            onClick: this.logTest
+            onClick: logTest//this.logTest
           }}
         />
       </div>
