f7c7ac9362580de40913851a2fcd08a1e211e1a5
Dayton Wilson <jdwilson73@Daytons-MacBook-Pro.local>
2020-07-27 12:12:19 -0400

    Cleaned up code

--- src/EnhancedTree.jsx
+++ src/EnhancedTree.jsx
@@ -3,25 +3,18 @@ import Tree from 'react-tree-graph';
 import { findFirst } from 'obj-traverse/lib/obj-traverse';
 
 export default class EnhancedTree extends React.Component {
+
   constructor(props) {
     super(props);
     const { data, height, width, svgProps } = this.props;
-    let count = 0;
-    this.state = {data, height, width, svgProps, count};
+    this.state = { data, height, width, svgProps };
   }
 
-/*
-  logTest(e, node) {
-    e.preventDefault();
-    alert(`Spooooop ${node}`);
-  }*/
-
   render() {
-    //const { data, height, width, svgProps } = this.props;
-    const { data, height, width, svgProps, count } = this.state;
-    const logTest = (e, node) => {
+    const { data, height, width, svgProps } = this.state;
+    const branchAdd = (e, node) => {
       e.preventDefault();
-      //alert(`Spooooop ${node}`);
+
       const newBranch = prompt("Enter new branch:", "Massaging a Jigglypuff");
       if (newBranch != null && newBranch != "") {
         let obj = findFirst(data, 'children', { name: node });
@@ -30,17 +23,13 @@ export default class EnhancedTree extends React.Component {
             name: `${newBranch}`,
             textProps: {transform: 'rotate(90)'},
           }];
-          //this.setState({count: count + 1});
         }
         else {
           obj.children.push({
             name: `${newBranch}`,
             textProps: {transform: 'rotate(90)'},
           });
-          //this.setState({count: count + 1});
         }
-        console.log(obj);
-        console.log(data);
         this.setState({data: data});
       }
     }
@@ -55,7 +44,7 @@ export default class EnhancedTree extends React.Component {
           svgProps={svgProps}
           gProps={{
             className: 'node',
-            onClick: logTest//this.logTest
+            onClick: branchAdd
           }}
         />
       </div>
